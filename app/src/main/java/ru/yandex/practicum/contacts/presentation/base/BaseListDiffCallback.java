c55f4038949e433859c4915a2ec43f3f7beb371a
KozlovDD91 <louisdegidros@yandex.ru>
2023-02-16 08:39:34 +0300

    Исправил замечания.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -11,6 +11,12 @@ public class BaseListDiffCallback <T extends ListDiffInterface<T>> extends DiffU
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return false;
+
+        return oldItem.equals(newItem);
+    }
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
     }
 }
+
